126d95f8dcb6ffc4d77f6e82b1c303d5bf220626
Brian Glowe <brianglowe@gmail.com>
2015-10-19 16:02:18 -0400

    Setting up Navigation Controller

---
+++ BootcampGroupProjectWeekOne/BootcampGroupProjectWeekOne/Gameplay.swift
@@ -0,0 +1,13 @@
+//
+//  Gameplay.swift
+//  BootcampGroupProjectWeekOne
+//
+//  Created by Brian J Glowe on 10/19/15.
+//  Copyright © 2015 Brian Amelia Kate. All rights reserved.
+//
+
+import Foundation
+
+class Gameplay {
+    var name: String = ""
+}
---
+++ BootcampGroupProjectWeekOne/Celebrity.swift
@@ -0,0 +1,23 @@
+//
+//  Celebrity.swift
+//  BootcampGroupProjectWeekOne
+//
+//  Created by Brian J Glowe on 10/19/15.
+//  Copyright © 2015 Brian Amelia Kate. All rights reserved.
+//
+
+import Foundation
+
+struct Celebrity {
+    let celebrityOptions = [
+        [   "name"  : "Kanye",
+            "Tweets" : ["I am the king", "I like golddiggers", "I am a little man", "I like fish dips"]
+        ],
+        [   "name"  : "Donald",
+            "Tweets" : ["I am the queen", "I like hotels", "I have great hair", "I like walls"]
+        ],
+        [   "name"  : "Ted Nugent",
+            "Tweets" : ["Deers deserve to die", "America!", "Rock and roll", "Party in the USA"]
+        ],
+    ]
+}
